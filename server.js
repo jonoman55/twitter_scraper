cd56f167c43b38a46d87accd8b9b6e681e0c0052
jchiappetta <jchiappetta@devexperts.com>
2021-11-02 08:10:55 -0400

    Update server.js
    TODO's added and .env file added

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 require('dotenv').config();
 const tweets = require('./user_tweets');
 
+// TODO : Add more examples from: https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/
+// TODO : convert to function with usernames variable 
 tweets();
 
 const PORT = 5000;
